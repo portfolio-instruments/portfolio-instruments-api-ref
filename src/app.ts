554e73c03ab63afdeed2c9cafa8e9338a0691a10
MicroFish91 <mwfisher91@gmail.com>
2023-02-20 10:30:35 -0800

    Initialize server setup

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import helmet from "helmet";
+import compression from "compression";
+import morgan from "morgan";
+import { combinedRouter } from "./routes";
+
+const app = express();
+
+app.use(helmet());
+app.use(compression());
+
+// Body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+if (app.get("env") === "development") {
+  app.use(morgan("tiny"));
+}
+
+app.get("/", (_, res) => {
+  res.send("Welcome placeholder...");
+});
+
+combinedRouter(app);
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
---
+++ src/routes/index.ts
@@ -0,0 +1,3 @@
+import { type Express } from "express";
+
+export const combinedRouter = (_app: Express): void => {};
