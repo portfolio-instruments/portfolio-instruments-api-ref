6251cfa3e13b8594066e9942049581e4da38e3dd
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2023-05-03 23:47:10 -0700

    Basic test setup config with Jest (#12)

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import compression from 'compression';
-import express, { NextFunction, Request, Response } from 'express';
+import express, { Express, NextFunction, Request, Response } from 'express';
 import helmet from 'helmet';
 import config from './config';
 import processErrorHandlers from './errors/processErrorHandler';
@@ -10,47 +10,53 @@ import routes from './routes';
 import Logger from './utils/Logger';
 import swaggerDocs from './utils/swaggerDocs';
 
-const app = express();
-
-/** Process error handlers */
-processErrorHandlers();
-
-/** Log inbound and outbound traffic */
-app.use(trafficLogger);
-
-/** Basic security & performance  */
-app.use(helmet());
-app.use(compression());
-
-/** Body parser */
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-/** Swagger docs */
-swaggerDocs(app);
-
-/** API rules */
-app.use((req: Request, res: Response, next: NextFunction): void => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    res.status(200).json({});
-    return;
-  }
-
-  next();
-});
-
-/** Deserialize user */
-app.use(deserializeUser);
-
-/** Routes */
-routes(app);
-
-/** Error handling */
-app.use(notFoundHandler);
-app.use(errorFallbackHandler);
-
+const app: Express = createServer();
 app.listen(config.PORT, () => Logger.info(`App server started on port ${config.PORT}`));
+
+/** Core server instance setup before port listening */
+export function createServer(): Express {
+  const app = express();
+
+  /** Process error handlers */
+  processErrorHandlers();
+
+  /** Log inbound and outbound traffic */
+  app.use(trafficLogger);
+
+  /** Basic security & performance  */
+  app.use(helmet());
+  app.use(compression());
+
+  /** Body parser */
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: false }));
+
+  /** Swagger docs */
+  swaggerDocs(app);
+
+  /** API rules */
+  app.use((req: Request, res: Response, next: NextFunction): void => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+      res.status(200).json({});
+      return;
+    }
+
+    next();
+  });
+
+  /** Deserialize user */
+  app.use(deserializeUser);
+
+  /** Routes */
+  routes(app);
+
+  /** Error handling */
+  app.use(notFoundHandler);
+  app.use(errorFallbackHandler);
+
+  return app;
+}
