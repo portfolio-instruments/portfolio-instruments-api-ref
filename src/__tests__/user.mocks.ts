b0c243491347ac8255e66687fc54587f6eb17019
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2023-05-07 22:37:31 -0700

    Expand user test coverage (#13)

---
+++ src/__tests__/user.mocks.ts
@@ -0,0 +1,31 @@
+import { Settings, User } from '@prisma/client';
+import { omit } from 'lodash';
+
+const createUserRequest = {
+  email: 'test@example.com',
+  name: 'Bobby Newport',
+  password: 'abcd1234',
+  confirmPassword: 'abcd1234',
+};
+
+const createSettingsPayload: Settings = {
+  id: 1,
+  userId: 1,
+  vpThreshold: 10,
+  rebalanceThreshold: 10,
+  createdAt: '2021-09-30T13:31:07.674Z' as unknown as Date,
+  updatedAt: '2021-09-30T13:31:07.674Z' as unknown as Date,
+};
+
+const createUserPayload: User & { settings: Omit<Settings, 'id' | 'userId'> } = {
+  id: 1,
+  email: 'test@example.com',
+  name: 'Bobby Newport',
+  password: 'abcd1234',
+  role: 'USER',
+  settings: { ...omit(createSettingsPayload, ['id', 'userId']) },
+  createdAt: '2021-09-30T13:31:07.674Z' as unknown as Date,
+  updatedAt: '2021-09-30T13:31:07.674Z' as unknown as Date,
+};
+
+export default { createUserRequest, createUserPayload, createSettingsPayload };
--- src/__tests__/user.test.ts
+++ src/__tests__/user.test.ts
@@ -1,21 +1,54 @@
+import { omit } from 'lodash';
 import supertest from 'supertest';
 import { createServer } from '../app';
+import * as UserService from '../modules/user/user.service';
+import Mocks from './user.mocks';
 
 const app = createServer();
 
-describe('user', () => {
+describe('User', () => {
   /** POST /users */
-  describe('post users route', () => {
-    // Todo
+  describe('Register user', () => {
+    describe('Given the user is unique, and the username/password are valid', () => {
+      it('should succeed and return the user payload', async () => {
+        const getUserServiceMock = jest.spyOn(UserService, 'getUser').mockResolvedValueOnce(null);
+        const createUserServiceMock = jest.spyOn(UserService, 'createUser').mockResolvedValueOnce(omit(Mocks.createUserPayload, 'settings'));
+        const createSettingsServiceMock = jest.spyOn(UserService, 'createUserSettings').mockResolvedValueOnce(Mocks.createSettingsPayload);
+
+        const { statusCode, body } = await supertest(app).post('/v1/users').send(Mocks.createUserRequest);
+        expect(statusCode).toBe(201);
+        expect(body).toEqual(omit(Mocks.createUserPayload, 'password'));
+
+        expect(getUserServiceMock).toHaveBeenCalledWith(Mocks.createUserPayload.email);
+        expect(createSettingsServiceMock).toHaveBeenCalledWith(Mocks.createUserPayload.id);
+
+        // Password will look different since we hash it before creating
+        expect(createUserServiceMock).toHaveBeenCalledWith(
+          expect.objectContaining({
+            ...omit(Mocks.createUserRequest, 'confirmPassword'),
+            password: expect.not.stringMatching(Mocks.createUserRequest.password),
+          })
+        );
+      });
+    });
+
+    describe('Given the user is not unique', () => {
+      it('should return a 409', async () => {
+        const getUserServiceMock = jest.spyOn(UserService, 'getUser').mockResolvedValueOnce(Mocks.createUserPayload);
+        const { statusCode } = await supertest(app).post('/v1/users').send(Mocks.createUserRequest);
+        expect(statusCode).toBe(409);
+        expect(getUserServiceMock).toHaveBeenCalledWith(Mocks.createUserPayload.email);
+      });
+    });
   });
 
   /** GET /users */
-  describe('get users route', () => {
-    describe('given the user is logged in', () => {
+  describe('Find user', () => {
+    describe('Given the user is logged in', () => {
       // Todo
     });
 
-    describe('given the user is not logged in', () => {
+    describe('Given the user is not logged in', () => {
       it('should return a 401', async () => {
         const { statusCode } = await supertest(app).get('/v1/users');
         expect(statusCode).toBe(401);
