3d6ca5a8419e4aaed7b537202c8dcb70e354a72f
MicroFish91 <mwfisher91@gmail.com>
2023-05-28 09:37:22 -0700

    Test impl

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFiles: ['<rootDir>/.jest/setEnvVars.ts'],
   testMatch: ["**/*.test.ts"],
   testPathIgnorePatterns: ["/node_modules/", "/dist/"],
   verbose: true,
