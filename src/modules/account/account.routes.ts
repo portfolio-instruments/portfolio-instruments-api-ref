0adc5ebee9389d9504725ec8f55ec9ab1ab05624
MicroFish91 <mwfisher91@gmail.com>
2023-06-02 10:55:49 -0600

    Impl

--- src/modules/account/account.routes.ts
+++ src/modules/account/account.routes.ts
@@ -7,6 +7,8 @@ import accountController from './account.controller';
 
 const router = express.Router();
 
+router.get('/', validateRequest(), requireUser, asyncWrapper(accountController.getAllAccountsHandler));
+
 router.post('/', validateRequest(createAccountRequestSchema), requireUser, asyncWrapper(accountController.createAccountHandler));
 
 export default router;
--- src/modules/account/account.service.ts
+++ src/modules/account/account.service.ts
@@ -2,6 +2,10 @@ import { CreateAccountContext } from './account.schema';
 import prisma from '../../utils/prisma';
 import { Account } from '@prisma/client';
 
+export async function getAllAccounts(userId: number): Promise<Account[]> {
+  return await prisma.account.findMany({ where: { userId } });
+}
+
 export type NewAccount = CreateAccountContext & { userId: number };
 
 export async function createAccount(newAccount: NewAccount): Promise<Account> {
