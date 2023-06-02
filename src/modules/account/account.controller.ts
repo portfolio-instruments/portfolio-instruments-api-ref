b21df99a71acb2f8e19eb3eb7f66e4ea65217ca8
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2023-06-02 10:03:21 -0700

    Add `GET /accounts` route and implementation (#43)

--- src/modules/account/account.controller.ts
+++ src/modules/account/account.controller.ts
@@ -1,16 +1,21 @@
-import type { Response } from 'express';
+import type { Request, Response } from 'express';
 import type { CreateAccountContext, CreateAccountRequest } from './account.schema';
-import { createAccount } from './account.service';
+import { createAccount, getAllAccounts } from './account.service';
 import { ValidUserRequest } from '../../middleware/deserializeUser';
 import { nonNullValue } from '../../utils/nonNull';
 import { Account } from '@prisma/client';
-import { omit } from 'lodash';
+
+async function getAllAccountsHandler(req: ValidUserRequest & Request, res: Response): Promise<void> {
+  const userId: number = nonNullValue(req.locals?.user?.id);
+  const accounts: Account[] = await getAllAccounts(userId);
+  res.status(200).json(accounts);
+}
 
 async function createAccountHandler(req: ValidUserRequest & CreateAccountRequest, res: Response): Promise<void> {
   const userId: number = nonNullValue(req.locals?.user?.id);
   const accountContext: CreateAccountContext = req.body as CreateAccountContext;
   const account: Account = await createAccount({ ...accountContext, userId });
-  res.status(201).json(omit(account, 'userId'));
+  res.status(201).json(account);
 }
 
-export default { createAccountHandler };
+export default { createAccountHandler, getAllAccountsHandler };
