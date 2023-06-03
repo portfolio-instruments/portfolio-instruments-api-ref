ecf5daefa3d5992376a073325a57d1669a6c2a57
MicroFish91 <mwfisher91@gmail.com>
2023-06-03 13:21:21 -0600

    impl

--- src/modules/account/account.request.schema.ts
+++ src/modules/account/account.request.schema.ts
@@ -1,31 +1,45 @@
 import { TaxShelter } from '@prisma/client';
 import { coerce, nativeEnum, object, string, TypeOf } from 'zod';
 
+/** Query-able keys */
 export const queryAbleAccountKeys: string[] = ['id', 'institution', 'taxShelter', 'description', 'active', 'createdAt', 'updatedAt'];
+
+/** Base Schema */
+const bodySchema = object({
+  institution: string({ required_error: 'Institution is required' })
+    .min(2, 'Name must be at least 2 characters long')
+    .max(255, 'Name can be at most 255 characters long'),
+  taxShelter: nativeEnum(TaxShelter),
+  description: string(),
+  active: coerce.boolean({ invalid_type_error: 'The active status must be a boolean value' }).optional(),
+});
+
+const paramsSchema = object({
+  accountId: coerce
+    .number({ invalid_type_error: 'The accountId must be a positive whole number' })
+    .int('The accountId must be a whole number')
+    .positive('The accountId must be a positive number'),
+});
 
 /** Create Account */
 export const createAccountRequestSchema = object({
-  body: object({
-    institution: string({ required_error: 'Institution is required' })
-      .min(2, 'Name must be at least 2 characters long')
-      .max(255, 'Name can be at most 255 characters long'),
-    taxShelter: nativeEnum(TaxShelter),
-    description: string().optional(),
-    active: coerce.boolean({ invalid_type_error: 'The active status must be a boolean value' }).optional(),
-  }),
+  body: bodySchema,
 });
 
 export type CreateAccountRequest = TypeOf<typeof createAccountRequestSchema>;
 export type CreateAccountContext = CreateAccountRequest['body'];
 
+/** Edit Account */
+export const editAccountRequestSchema = object({
+  body: bodySchema.partial().strict(),
+  params: paramsSchema.strict(),
+});
+
+export type EditAccountRequest = { body: TypeOf<typeof editAccountRequestSchema>['body'] } & { params: { accountId: string } };
+
 /** Get Account By Id */
 export const getAccountByIdRequestSchema = object({
-  params: object({
-    accountId: coerce
-      .number({ invalid_type_error: 'The accountId must be a positive whole number' })
-      .int('The accountId must be a whole number')
-      .positive('The accountId must be a positive number'),
-  }),
+  params: paramsSchema,
 });
 
 export type GetAccountRequest = { params: { accountId: string } };
--- src/modules/account/account.routes.ts
+++ src/modules/account/account.routes.ts
@@ -2,13 +2,15 @@ import express from 'express';
 import asyncWrapper from '../../middleware/asyncWrapper';
 import { requireUser } from '../../middleware/requireRole';
 import validateRequest from '../../middleware/validateRequest';
-import { createAccountRequestSchema, getAccountByIdRequestSchema } from './account.request.schema';
+import { createAccountRequestSchema, editAccountRequestSchema, getAccountByIdRequestSchema } from './account.request.schema';
 import accountController from './account.controller';
 
 const router = express.Router();
 
 router.get('/', validateRequest(), requireUser, asyncWrapper(accountController.getAllAccountsHandler));
 router.get('/:accountId', validateRequest(getAccountByIdRequestSchema), requireUser, asyncWrapper(accountController.getAccountByIdHandler));
+
+router.patch('/:accountId', validateRequest(editAccountRequestSchema), requireUser, asyncWrapper(accountController.editAccountHandler));
 
 router.post('/', validateRequest(createAccountRequestSchema), requireUser, asyncWrapper(accountController.createAccountHandler));
 
