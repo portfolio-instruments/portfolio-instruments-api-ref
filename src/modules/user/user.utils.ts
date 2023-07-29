19bb1bd73fd369311b1bff992df9cd39b3424e7e
MicroFish91 <mwfisher91@gmail.com>
2023-07-28 21:41:18 -0700

    Remove parseCreateUser

--- src/modules/user/user.utils.ts
+++ src/modules/user/user.utils.ts
@@ -1,13 +1,5 @@
 import bcrypt from 'bcryptjs';
 import config from '../../config';
-import type { CreateUserRequest } from './user.request.schema';
-import type { CreateUserContext } from './user.service';
-
-export function parseCreateUser(req: CreateUserRequest): CreateUserContext {
-  const { email, name, password } = req.body;
-  const hashedPassword: string = hashPassword(password);
-  return { email, name, password: hashedPassword };
-}
 
 export function hashPassword(password: string): string {
   const salt: string = bcrypt.genSaltSync(config.SALT_WORK_FACTOR);
