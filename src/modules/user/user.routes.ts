c6b48c2766b9c4e940f569ab32dbeee2615a9589
Matthew Fisher <40250218+MicroFish91@users.noreply.github.com>
2023-08-02 00:28:12 -0700

    GET `/user/:userId/settings` swagger docs (#110)

--- src/modules/user/user.routes.ts
+++ src/modules/user/user.routes.ts
@@ -46,6 +46,57 @@ router.post('/', validateRequest(createUserRequestSchema), asyncWrapper(userCont
 /** Read */
 router.get('/', validateRequest(), requireUser, asyncWrapper(userController.getUsersHandler));
 
+/**
+ * @openapi
+ * /users/{userId}/settings:
+ *    get:
+ *      summary: Retrieve a list of user settings
+ *      tags:
+ *        - User
+ *      security:
+ *        - bearerAuth: []
+ *      parameters:
+ *       - $ref: '#/components/parameters/userId'
+ *      responses:
+ *          200:
+ *              description: Successfully retrieved a list of user settings
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/GetUserSettingsResponse'
+ *          400:
+ *              description: Bad Request
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Error'
+ *          401:
+ *              description: Unauthorized
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Error'
+ *          403:
+ *              description: Forbidden
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Error'
+ *          404:
+ *              description: Not Found
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: '#/components/schemas/Error'
+ *
+ * components:
+ *   parameters:
+ *     userId:
+ *         name: userId
+ *         in: path
+ *         description: The id of user
+ *         required: true
+ */
 router.get(
   '/:userId/settings',
   validateRequest(getUserSettingsByIdRequestSchema),
